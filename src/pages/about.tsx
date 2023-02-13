a367ddd0a141bb8c6524e240f21cf2535a2bfc3c
konyu <yusukedrive@gmail.com>
2023-02-13 17:01:12 +0900

    Add: ESlintに怒られるコード追加

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,8 +1,15 @@
 const About = () => {
   const greeting = "hello";
 
+  console.log("foo");
   return (
     <>
+      <img
+        src="https://example.com/hero.jpg"
+        alt="Landscape picture"
+        width={800}
+        height={500}
+      />
       <h1>About me</h1>
       <h2>{greeting}</h2>
     </>
