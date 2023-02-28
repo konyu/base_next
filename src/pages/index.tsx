7c31342ff05745cf51712aa45e13e05e7ad7ec6f
konyu <yusukedrive@gmail.com>
2023-02-28 10:44:46 +0900

    Fix: npm ci

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,8 @@ import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
-
+const b = 2
 const a = 1
-
-
-
-
 
 export default function Home() {
   return (
